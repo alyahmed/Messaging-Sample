1f426248740553b3b23270adc1cb03062bdfcb5d
Ahmed Aly <alyahmed220@gmail.com>
2015-06-02 23:33:06 -0400

    Added pub/sub classes

--- src/main/java/Main.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by ahmedaly on 15-06-02.
- */
-public class Main {
-}
---
+++ src/main/java/Send.java
@@ -0,0 +1,30 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by ahmedaly on 15-06-02.
+ */
+public class Send {
+
+    private final static String QUEUE_NAME = "hello";
+
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        for (int i = 1; i < 100; i++) {
+            String message = "Hello from Producer: message #" + i;
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+        channel.close();
+        connection.close();
+    }
+}
